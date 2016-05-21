307f6c3c6497387875c1732980e97012e90a898e
unknown <Maksim.Superfin@IL-MAKSIMS-QA.Kaltura.com>
2016-05-21 22:52:21 +0300

    Added ListView into project to check how categories are processed

--- Android_app/app/src/main/java/com/grest/gip/SearchResults.java
+++ Android_app/app/src/main/java/com/grest/gip/SearchResults.java
@@ -6,7 +6,12 @@ import android.os.Bundle;
 import android.support.v7.widget.AppCompatButton;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
+
+import com.grest.gip.com.grest.gip.dao.Categories;
 
 /**
  * Created by Maksim.Superfin on 5/13/2016.
@@ -28,7 +33,22 @@ public class SearchResults extends AppCompatActivity {
                 displayResults(v);
             }
         });
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, Categories.categories);
+        ListView listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(mMessageClickedHandler);
     }
+
+    // Create a message handling object as an anonymous class.
+    private AdapterView.OnItemClickListener mMessageClickedHandler = new AdapterView.OnItemClickListener() {
+        public void onItemClick(AdapterView parent, View v, int position, long id) {
+            // Do something in response to the click
+            EditText editText = (EditText) findViewById(R.id.editText);
+            editText.setText(Categories.categories2Abbrevations.get((CharSequence) parent.getAdapter().getItem(position)));
+        }
+    };
 
     public void displayResults(View view) {
         Intent intent = new Intent(SearchResults.this, MapsActivity.class);
--- Android_app/app/src/main/java/com/grest/gip/com/grest/gip/dao/Categories.java
+++ Android_app/app/src/main/java/com/grest/gip/com/grest/gip/dao/Categories.java
@@ -2,6 +2,7 @@ package com.grest.gip.com.grest.gip.dao;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by Maksim.Superfin on 5/13/2016.
@@ -10,10 +11,33 @@ public class Categories {
     public static Map<String, String> categories2Abbrevations = new HashMap<String, String>();
     static {
         categories2Abbrevations.put("Food & Drink", "food-and-drink");
+        categories2Abbrevations.put("Beauty & spas", "beauty-and-spas");
+        categories2Abbrevations.put("Women", "women");
+        categories2Abbrevations.put("Men", "men");
+        categories2Abbrevations.put("Home", "home-improvement|auto-and-home-improvement|food-and-drink-goods|home-and-garden-goods|household-essentials");
+        categories2Abbrevations.put("Kids", "baby-kids-and-toys");
     }
 
     public static Map<String, String> abbrevations2Categories = new HashMap<String, String>();
     static {
         abbrevations2Categories.put("food-and-drink", "Food & Drink");
+        abbrevations2Categories.put("beauty-and-spas", "Beauty & spas");
+        abbrevations2Categories.put("women", "Women");
+        abbrevations2Categories.put("men", "Men");
+        abbrevations2Categories.put("home-improvement|auto-and-home-improvement|food-and-drink-goods|home-and-garden-goods|household-essentials", "Home");
+        abbrevations2Categories.put("baby-kids-and-toys", "Kids");
+    }
+
+    public static String[] categories = getCategoriesNames();
+
+    private static String[] getCategoriesNames() {
+
+        Set<String> keys = categories2Abbrevations.keySet();
+        String[] result = new String[keys.size()];
+        int i = 0;
+        for (String key: keys) {
+            result[i++] = key;
+        }
+        return result;
     }
 }
