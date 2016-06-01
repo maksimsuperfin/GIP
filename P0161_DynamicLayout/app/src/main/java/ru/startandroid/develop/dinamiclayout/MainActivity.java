d4d3755c30187ebf6a2deb1098e95c7ffd4ad2a0
unknown <Maksim.Superfin@IL-MAKSIMS-QA.Kaltura.com>
2016-06-01 21:54:55 +0300

    Updated logic related menu processing

--- P0161_DynamicLayout/app/src/main/java/ru/startandroid/develop/dinamiclayout/MainActivity.java
+++ P0161_DynamicLayout/app/src/main/java/ru/startandroid/develop/dinamiclayout/MainActivity.java
@@ -1,65 +1,77 @@
 package ru.startandroid.develop.dinamiclayout;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.GridView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends Activity {
-
-        GridView gridView;
-        Button btn;
-        static final String[] items_main = new String[] {
-                "Food and Drink", "Shop", "Spa/hear style", "Vocations"};
-        static final String[] items_food = new String[] {
-            "Bars", "Restaurants", "Cafe"};
-        static final String[] items_spa = new String[] {
+    GridView gridView;
+    Button btn;
+    ArrayAdapter<String> arrayAdapter;
+    boolean isFirstMenuOpened = false;
+    static final List<String> itemsMain = new ArrayList<String>();
+    static {
+        itemsMain.add("Food and Drink");
+        itemsMain.add("Shop");
+        itemsMain.add("Spa/hear style");
+        itemsMain.add("Vocations");
+    }
+    static final List<String> itemsFood = new ArrayList<String>();
+    static {
+        itemsFood.add("Bars");
+        itemsFood.add("Restaurants");
+        itemsFood.add("Cafe");
+    }
+    static final String[] itemsSpa = new String[]{
             "Spa", "Hears stile", "Mani and Pedicure", "Vocations"};
-        static final String[] items_shop = new String[] {
+    static final String[] itemsShop = new String[]{
             "Men", "Women", "Kids", "For Home"};
-        static final String[] items_vacation = new String[] {
+    static final String[] itemsVacation = new String[]{
             "Local", "Abroad", "Attractions"};
-        static final String[][] totalmenu = new String[][]{items_main,items_shop,items_food,items_spa, items_vacation};
+    static final Map<String, List<String>> totalMenu = new HashMap<String, List<String>>();
+    static {
+        totalMenu.put("Food and Drink", itemsFood);
+    }
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-            gridView= (GridView) findViewById(R.id.gridview);
-            for(int i=0;i<totalmenu.length;i++){
-            ArrayAdapter<String> arrayAdapter=new ArrayAdapter<String>(this, R.layout.list_item, totalmenu[i]);
-            gridView.setAdapter(arrayAdapter);
-            }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        gridView = (GridView) findViewById(R.id.gridview);
+        arrayAdapter = new ArrayAdapter<String>(this, R.layout.list_item, itemsMain);
+        gridView.setAdapter(arrayAdapter);
 
-            gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                public void onItemClick(AdapterView<?> parent, View v,
-                                        int position, long id) {
-                   String[] tmp=getNextmenu(position);
-
-                    ArrayAdapter<String> arad=new ArrayAdapter<String>(this,R.layout.list_item,tmp);
-                    gridView.setAdapter(arad);
-                     /* Toast.makeText(getApplicationContext(),
-                            ((TextView) v).getText(), Toast.LENGTH_SHORT).show();*/
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                if (isFirstMenuOpened) {
+                    Toast.makeText(getApplicationContext(),
+                            ((TextView) v).getText(), Toast.LENGTH_SHORT).show();
+                } else {
+                    List<String> tmp = totalMenu.get(((TextView) v).getText());
+                    arrayAdapter.clear();
+                    arrayAdapter.addAll(tmp);
+                    arrayAdapter.notifyDataSetChanged();
+                    isFirstMenuOpened = true;
                 }
-            });
-
-            btn= (Button) findViewById(R.id.button);
-            btn.setText("My Home");
-
-
-        }
-
-    private String[] getNextmenu(int position) {
-        String[]tmp=new String[]{};
-        for(int j=position, i=0;i<totalmenu[position].length;i++){
-            String tmpstr=totalmenu[position][i].toString();
-
-            tmp[i]=tmpstr;
-        }
-        return tmp;
+            }
+        });
+        btn = (Button) findViewById(R.id.button);
+        btn.setText("My Home");
     }
 }
 
