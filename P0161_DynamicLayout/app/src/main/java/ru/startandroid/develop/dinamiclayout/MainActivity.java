2434dcaca665068a916604e309c4038e7ed406e1
unknown <Maksim.Superfin@IL-MAKSIMS-QA.Kaltura.com>
2016-06-03 14:56:30 +0300

    Implemented logic for "Back" button Removed useless souts

--- P0161_DynamicLayout/app/src/main/java/ru/startandroid/develop/dinamiclayout/MainActivity.java
+++ P0161_DynamicLayout/app/src/main/java/ru/startandroid/develop/dinamiclayout/MainActivity.java
@@ -33,7 +33,6 @@ public class MainActivity extends Activity {
             @TargetApi(Build.VERSION_CODES.HONEYCOMB)
             public void onItemClick(AdapterView<?> parent, View v,
                                     int position, long id) {
-                System.out.println("getText() ::: " + ((TextView) v).getText());
                 if (isMainMenuOpened) {
                     List<String> tmp = MenuConstants.TOTAL_MENU.get(((TextView) v).getText());
                     updateAdapterBy(tmp);
@@ -42,7 +41,6 @@ public class MainActivity extends Activity {
                     Toast.makeText(getApplicationContext(),
                             ((TextView) v).getText(), Toast.LENGTH_SHORT).show();
                 }
-                System.out.println("gridView.setOnItemClickListener ::: " + MenuConstants.ITEMS_MAIN);
             }
         });
         btnBack = (Button) findViewById(R.id.button_back);
@@ -50,65 +48,21 @@ public class MainActivity extends Activity {
         View.OnClickListener backBtnClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                System.out.println("back is clicked");
                 if (!isMainMenuOpened) {
-                    System.out.println("Adapter is updated ::: " + MenuConstants.ITEMS_MAIN);
                     updateAdapterBy(MenuConstants.ITEMS_MAIN);
                     isMainMenuOpened = true;
                 }
-                System.out.println("onClick ::: " + MenuConstants.ITEMS_MAIN);
             }
         };
         btnBack.setOnClickListener(backBtnClickListener);
         btn = (Button) findViewById(R.id.button);
         btn.setText("My Home");
-        System.out.println("onCreate ::: " + MenuConstants.ITEMS_MAIN);
     }
 
     @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     private void updateAdapterBy(List<String> items) {
-        System.out.println("before updateAdapterBy ::: " + items);
-        arrayAdapter.clear();
-        arrayAdapter.notifyDataSetChanged();
-        arrayAdapter.addAll(items);
-        arrayAdapter.notifyDataSetChanged();
-        System.out.println("updateAdapterBy ::: " + MenuConstants.ITEMS_MAIN);
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        System.out.println("onStart ::: " + MenuConstants.ITEMS_MAIN);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        System.out.println("onResume ::: " + MenuConstants.ITEMS_MAIN);
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        System.out.println("onPause ::: " + MenuConstants.ITEMS_MAIN);
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        System.out.println("onStop ::: " + MenuConstants.ITEMS_MAIN);
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        System.out.println("onDestroy ::: " + MenuConstants.ITEMS_MAIN);
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        System.out.println("onRestart ::: " + MenuConstants.ITEMS_MAIN);
+        arrayAdapter = new ArrayAdapter<String>(this, R.layout.list_item, items);
+        gridView.setAdapter(arrayAdapter);
     }
 }
 
