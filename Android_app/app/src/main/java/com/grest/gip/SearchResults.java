f6257e8cddbf0e6ec67d2f9975b90f7f4f970bb6
maksimsuperfin <maxym.superfin@gmail.com>
2016-06-14 22:31:51 +0300

    completed updates related gridView in menu

--- Android_app/app/src/main/java/com/grest/gip/SearchResults.java
+++ Android_app/app/src/main/java/com/grest/gip/SearchResults.java
@@ -15,7 +15,6 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
-import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -42,15 +41,8 @@ public class SearchResults extends AppCompatActivity implements PopupMenu.OnMenu
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_results);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, GrouponConstants.categories);
-        /*ListView listView = (ListView) findViewById(R.id.listView);
-        listView.setAdapter(adapter);
-        listView.setOnItemClickListener(mMessageClickedHandler);*/
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
         intent = getIntent();
         country = intent.getStringExtra(SearchResults.COUNTRY_EXTRA_MESSAGE);
         if (country == null) {
@@ -68,11 +60,13 @@ public class SearchResults extends AppCompatActivity implements PopupMenu.OnMenu
                     List<String> tmp = GrouponConstants.TOTAL_MENU.get(((TextView) v).getText());
                     if (tmp != null) {
                         updateAdapterBy(tmp);
+                        displayNavigationOnTolbar();
+                    } else {
+                        displayResults(parent, position);
                     }
                     isMainMenuOpened = false;
                 } else {
-                    Toast.makeText(getApplicationContext(),
-                            ((TextView) v).getText(), Toast.LENGTH_SHORT).show();
+                    displayResults(parent, position);
                 }
             }
         });
@@ -89,23 +83,25 @@ public class SearchResults extends AppCompatActivity implements PopupMenu.OnMenu
         toolbar.setNavigationOnClickListener(backBtnClickListener);
     }
 
+    private void displayNavigationOnTolbar() {
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        toolbar.getNavigationIcon().setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_ATOP);
+    }
+
+    private void displayResults(AdapterView<?> parent, int position) {
+        Intent intent = new Intent(SearchResults.this, MapsActivity.class);
+        String message = GrouponConstants.categories2Abbrevations.get(
+                (CharSequence) parent.getAdapter().getItem(position));
+        intent.putExtra(CATEGORY_EXTRA_MESSAGE, message);
+        intent.putExtra(COUNTRY_EXTRA_MESSAGE, country);
+        startActivity(intent);
+    }
+
     private void updateAdapterBy(List<String> items) {
         arrayAdapter = new ArrayAdapter<String>(this, R.layout.list_item, items);
         gridView.setAdapter(arrayAdapter);
     }
-
-    // Create a message handling object as an anonymous class.
-    /*private AdapterView.OnItemClickListener mMessageClickedHandler = new AdapterView.OnItemClickListener() {
-        public void onItemClick(AdapterView parent, View v, int position, long id) {
-            // Do something in response to the click
-            Intent intent = new Intent(SearchResults.this, MapsActivity.class);
-            String message = GrouponConstants.categories2Abbrevations.get(
-                    (CharSequence) parent.getAdapter().getItem(position));
-            intent.putExtra(CATEGORY_EXTRA_MESSAGE, message);
-            intent.putExtra(COUNTRY_EXTRA_MESSAGE, country);
-            startActivity(intent);
-        }
-    };*/
 
     public void showCountries(View v) {
         PopupMenu popup = new PopupMenu(this, v, Gravity.RIGHT);
@@ -123,8 +119,10 @@ public class SearchResults extends AppCompatActivity implements PopupMenu.OnMenu
         inflater.inflate(R.menu.app_menu, menu);
         toolbar.setTitleTextColor(Color.WHITE);
         toolbar.setSubtitleTextColor(Color.WHITE);
+        if (!isMainMenuOpened) {
+            displayNavigationOnTolbar();
+        }
         toolbar.getOverflowIcon().setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_ATOP);
-        toolbar.getNavigationIcon().setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_ATOP);
         return true;
     }
 
